07c1b84e6c4aff508dcd7c4f1268f9d605e75b00
Toby Martineau <tobymartineau@live.com>
2015-05-10 23:19:48 -0600

    Revert "Revert "Actions on the C# side can be used as callbacks""
    This reverts commit 97b6394a461b21955447f9dd3a64b87dc9c282de.

--- Assets/Resources/Scripts/CallbackDict.cs
+++ Assets/Resources/Scripts/CallbackDict.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
 using MoonSharp.Interpreter;
+using UnityEngine;
 
 namespace BulletHell
 {
 	public class CallbackDict
 	{
-		private Script script = new Script ();
+		private List<DynValue> funcs = new List<DynValue> ();
+
+		private Script script;
 
 		public CallbackDict (Script newscript)
 		{
@@ -18,13 +21,31 @@ namespace BulletHell
 			}
 		}
 
+		public void SetObject(string key, object obj) {
+			// I feel ashamed of this code
+
+			DynValue func = DynValue.FromObject (script, obj);
+
+			funcs.Add (func);
+
+			script.Globals ["funcs"] = funcs;
+
+			DynValue value = script.DoString (
+				"return function (...)\n" +
+				"\tfuncs[" + (funcs.Count - 1).ToString() +"].Invoke(...)\n" +
+                "end"
+			);
+
+			this [key].Add (value);
+		}
+
 		private Dictionary<string, List<DynValue>> callbacks = new Dictionary<string, List<DynValue>> ();
 
 		public List<DynValue> this[string key] {
 			get {
-				if (!callbacks.ContainsKey(key)) {
+				if (!callbacks.ContainsKey(key))
 					callbacks[key] = new List<DynValue> ();
-				}
+
 				return callbacks[key];
 			}
 		}
